5d8514d01735017266b00a92a5761971312ef240
DoneeX <vlad.kupratyi@gmail.com>
2015-08-02 22:03:26 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Test task</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="../assets/sss.min.js"></script>
+    <link rel="stylesheet" href="../assets/sss.css" type="text/css" media="all">
+    <script>
+        jQuery(function($) {
+            $('.slider').sss({
+            });
+        });
+    </script>
+</head>
+<body>
+
+</body>
+</html>
